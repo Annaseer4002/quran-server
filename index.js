1600e24e9fd96217eea66d679d920cfcb53626fd
Abdulnasir Badamasi <abdulnasirbadamasi5@gmail.com>
2026-05-19 14:05:30 +0100

    fix port access

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.get('/api/recitation/:recitationId', async (req, res) => {
 
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
